c1e4d78427119619af4510f8c8fd1ca4a98f9dbc
Markus Heikkinen <markush@softco.com>
2018-06-16 08:51:53 +0300

    Vaadin8 compatibility changes

--- resetbuttonfortextfield-demo/src/main/java/org/vaadin/resetbuttonfortextfield/DemoUI.java
+++ resetbuttonfortextfield-demo/src/main/java/org/vaadin/resetbuttonfortextfield/DemoUI.java
@@ -4,18 +4,21 @@ import java.util.Date;
 
 import javax.servlet.annotation.WebServlet;
 
+import org.vaadin.addons.resetbuttonfortextfield.ResetButtonClickListener;
+import org.vaadin.addons.resetbuttonfortextfield.ResetButtonForTextField;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.data.Property;
-import com.vaadin.event.FieldEvents;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
+import com.vaadin.v7.data.Property;
+import com.vaadin.v7.event.FieldEvents;
+import com.vaadin.v7.ui.Label;
+import com.vaadin.v7.ui.TextField;
 
 @Theme("valo")
 @Title("Reset Button for TextField Extension Demo")
